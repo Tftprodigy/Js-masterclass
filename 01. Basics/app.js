4b354a0a94aae98c5d4b53245ea96bc05812963d
tftprodigy <Olurombisammy@gmail.com>
2024-08-03 17:09:50 +0100

    01. basics (console)

---
+++ 01. Basics/app.js
@@ -0,0 +1,13 @@
+// // 1. CONSOLE 
+
+// console.log("Hello Developer");
+// console.log("lets keep writing code");
+// let userName = "Dev";
+// console.log(userName);
+// console.log("Hello there", userName);
+
+// console.error("404 not found");
+// console.table({ name: "Olayiwola", age: 25, role: "leadDev" });
+
+// console.warn('this is a test warning');
+
