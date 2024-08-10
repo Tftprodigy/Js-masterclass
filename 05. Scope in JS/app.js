db15943e15de5b354030851dcf6aa43371ee8657
tftprodigy <Olurombisammy@gmail.com>
2024-08-10 05:08:32 +0100

    5.Scopes( Date con, intervals)

--- 05. Scope in JS/app.js
+++ 05. Scope in JS/app.js
@@ -8,7 +8,7 @@
 // }
 // showMessage();
 
-// 2.0. METHODS IN JS
+// #2.0. METHODS IN JS
 // these are functions inside an object.
 
 // const person = {
@@ -45,31 +45,101 @@
 
 // console.log(worker.workerRecap());
 
-// JSON
+// ###JSON
 // JSON: JavaScript Object Notation. is an open data interchange
 // format that is both human and machine-readable.
 
 //since this is a js file, it is converted back into being
 // a regular object. we can also convert this regular object
 // to json code and vice versa using two methods.
-const personJsonTest = {
-  name: "Universal Dev Ola",
-  age: 25,
-  email: null,
-  hobbies: ["coding", "sports", "Cooking"],
-  adress: {
-    city: "surulere",
-    House: 77,
-    confirmed: true,
-  },
-};
+// const personJsonTest = {
+//   name: "Universal Dev Ola",
+//   age: 25,
+//   email: null,
+//   hobbies: ["coding", "sports", "Cooking"],
+//   adress: {
+//     city: "surulere",
+//     House: 77,
+//     confirmed: true,
+//   },
+// };
 
-//JSON.stringify()
-const jsonString = JSON.stringify(personJsonTest);
-// this regular javascript object is now converted
-// into a json code.
-console.log(jsonString);
+// //##JSON.stringify()
+// const jsonString = JSON.stringify(personJsonTest);
+// // this regular javascript object is now converted
+// // into a json code.
+// console.log(jsonString);
 
-//JSON.parse
-const JsonParse = JSON.parse(jsonString);
-console.log(JsonParse);
+// //##JSON.parse
+// const JsonParse = JSON.parse(jsonString);
+// console.log(JsonParse);
+
+// ###DATE CONTRUCTOR
+// (year, month,day, hour, minute, seconds, milliseconds)
+// for example
+// const currentDate = new Date();
+// console.log(currentDate);
+
+// // now lets pass in our own date properties manually
+// const manualDate = new Date(2024, 7, 25, 12, 15, 0, 0);
+// console.log(manualDate);
+
+// const myDate = new Date();
+// // extracting each properties using their own methods from the
+// // dtae constructor.
+
+// const year = myDate.getFullYear();
+// const month = myDate.getMonth();
+// const hours = myDate.getHours();
+// let minutes = myDate.getMinutes();
+// const seconds = myDate.getSeconds();
+// const milliseconds = myDate.getMilliseconds();
+
+// console.log(`
+// Year: ${year}
+// Month: ${month}
+// Hours: ${hours}
+// minutes: ${minutes}
+// Seconds: ${seconds}
+// milliseconds: ${milliseconds}`);
+// ------------------------------------------------------------
+
+// ##SETINTERval IN JS
+//  is used to execute a function or a block of
+// code at a specified interval.
+//   synax:
+// setInterval(function(){
+//     ...code of function block
+// }, intervalBytime);
+
+// setInterval(function() {
+//   console.log('This function will execute every 2 secs');
+// }, 2000)
+
+// ##SETTIMEOUT (setTimeOut)
+// is used to execute a function or a block of
+// code after a specified delay.
+
+// setTimeOut(function(){
+// ...code of function block
+// }, timeOutInMilliseconds);
+
+// setTimeout(function () {
+//   console.log("This function will be executed after 3 seconds");
+// }, 3000);
+
+// ##CLEAR INTERVAL (clearInterval())
+// clearInterval() : is used to stop or break a setInterval format
+//                  being in an infinite loop.
+//                  it breaks out of the cycle of the setInterval.
+
+// A code being run at interval
+const intervalId = setInterval(function () {
+  console.log("This code is being executed at the rate of 1000 mili sec");
+}, 1000);
+
+//Stop the code from running
+setTimeout(function () {
+  clearInterval(intervalId);
+  console.log("this code is now stopped!");
+},10000);
