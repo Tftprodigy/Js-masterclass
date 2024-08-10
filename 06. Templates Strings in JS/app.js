2c30058ae81814e2f56f5526448d3165a58bf0ad
tftprodigy <Olurombisammy@gmail.com>
2024-08-10 05:34:03 +0100

    6. Template strings(` `)

---
+++ 06. Templates Strings in JS/app.js
@@ -0,0 +1,10 @@
+// 1. TEMPLATE STRING ASSIGNMENT
+
+console.log(`The quick
+brown fox
+jumps over
+the lazy dog`);
+
+let firstName = "UniversalDev";
+let lastName = "Ola";
+console.log(`Hello ${firstName} ${lastName }`);
