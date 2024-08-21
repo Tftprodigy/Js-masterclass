f06a74f626e8c8635b8a2799310bc81b83d2a774
tftprodigy <Olurombisammy@gmail.com>
2024-08-21 06:13:28 +0100

    6. Destructuring arrays, objects.

--- 06. Templates Strings in JS/app.js
+++ 06. Templates Strings in JS/app.js
@@ -218,6 +218,39 @@
 // // ............................................
 
 // // ###5.0 SPREAD OPERATOR
+// ##Spread Operators Arrays
+// const strNums = ["one", "two", "three"];
+// const moreStrNums = ["four", "five", "six"];
+// // lets use the spread operator to add/combine/ spread
+// //  these two arrays
+// const strNumConcat = [...strNums, ...moreStrNums];
+// console.log(strNumConcat);
+
+// // more spread operator using array example
+// let peoples = ["nukky", "kemi", "oyo"];
+// const morePeople = ["lelle", "lily", ...peoples, "becky"];
+// console.log(morePeople);
+
+// ##Spread Operators Objects
+// const obj1 = { x: 1, y: 2 };
+// const obj2 = { z: 3 };
+// const obj3 = { ...obj1, ...obj2 };
+
+// console.log(obj3);
+
+// // #more example
+// const person = {
+//   name: "Kemi",
+//   age: 19,
+//   gender: "female",
+// };
+
+// const clonePerson = { ...person };
+// // console.log(clonePerson);
+// const moreCloneProperties = { ...clonePerson, work: "Dev", location: "Lsg" };
+// // console.log(moreCloneProperties);
+
+// #Spread OPERATOR ASSIGNMENT.
 // let arr = [1, 2, 3];
 // let arr2 = [4, 5];
 // const clone = [...arr, ...arr2];
@@ -231,25 +264,74 @@
 // const userClone = {...user};
 // console.log(userClone);
 
-// ###6.0 DESTRUCTURING'
+// ###6.0 REST OPERATOR
+// BOTH rest and spread operators are denoted using '...'
+// lets declare an anonymous function that will take in userdata as
+// an arg and clg userdata
+// function user(userdata) {
+//     console.log(userdata);
+// }
+// if we pass a parameter into this arg, it is only meant to hold one
+// per variable or arguement in this case we need to pass multiple user
+// data as methods in the objects but should all be stored in the userdata.
+// we can use the rest operator '...' at the initial declaration
+
+// function user(...userData) {
+//   console.log(userData);
+// }
+// // user("Ola", 25, "Dev, Backend-Dev,");
+
+// function person(firstName, lastName, ...hobbies) {
+//   console.log("First Name: ", firstName);
+//   console.log("Last Name: ", lastName);
+//   console.log('Hobbies: ', hobbies);
+// }
+
+// person('Nike', 'Segun', 'coding', 'praying', 'reading')
+
+// 6.1 ASSIGNMENT (rEST OPERATOR)
+//create a function to take unlimited parameters & log it
+// function unlimited(...container) {
+// console.log(container);
+// }
+// unlimited('dog', 'shark', 'food', 'bed', 'emoji', 'etc')
+
+// ###7.0 DESTRUCTURING'
 // DESTRUCTURING: allows us to unppsck vslues from data structures (arrays,
 // objects) into seperate distinct variables
 
-// ##6.1Destrucing with Arrays
-const count = ["one", "two", "three"];
+// ##7.1Destrucing with Arrays
+// const count = ["one", "two", "three"];
 // we can destructure the array by assigning it to individual
 // varaibles like.
-const [green, blue, red] = count;
+// const [green, blue, red] = count;
 
 // console.log(green);
 // console.log(blue);
 // console.log(red);
 
-const nums = [1, 2, 3, 4];
-const [element1, element2, element3, element4] = nums;
-console.log(element1, element2, element3, element4);
+// const nums = [1, 2, 3, 4];
+// const [element1, element2, element3, element4] = nums;
+// console.log(element1, element2, element3, element4);
 
-// ##6.2 Destructuring Objects
+// const letter = ["ape", "bee", "cat", "dog"];
+// const [a, b, c, d] = letter;
+// console.log(a, b);
+// console.log(d, c);
+// console.log(a, c);
+// console.log(a, c);
+// console.log(d, b);
+
+// ##7.2Destrucing with Arrays ASSIGNMENT
+// const colors = ["red", "green", "blue", "yellow", "orange"];
+// // lets destructure this array
+// const [color1, color2, color3, ...otherColors] = colors;
+// console.log("Color 1: ", color1);
+// console.log("color 2: ", color2);
+// console.log("color 3: ", color3);
+// console.log("others: ", otherColors);
+
+// ##7.3 Destructuring Objects
 // syntax:
 // let obj = {
 // prop: 'value'
@@ -279,3 +361,60 @@ let {prop, prop} = obj;
 // let {fName, lName, middlename = "''", age} = person1;
 // console.log(middlename);
 // console.log(fName, age);
+
+// const student = {
+//     name: 'Buki',
+//     position: 'First',
+//     age: 16
+// }
+
+// const {name, position, age} = student;
+// console.log(name);
+// console.log(position);
+// console.log(age);
+// if (age <= 17) {
+// console.log(`${name}, you are still a minor`);
+// }else{
+//     console.log(`${name}, you are old enough`);
+// }
+
+// #7.4 Destrucing Objects ASSIGNMENT
+// const person = {
+//   name: "John Doe",
+//   age: 30,
+//   gender: "male",
+//   country: "NGN",
+// };
+
+// const {name, age, country} = person;
+// console.log(name);
+// console.log(age);
+// console.log(country);
+
+// # 7.5 Renaming variables in object destructuring.
+// const num = {
+//   x: 100,
+//   y: 200,
+// };
+// const {x: newX, y: newY} = num;
+// console.log(newX);
+// console.log(newY);
+
+// // Assignment 
+// const person = {
+//   name: "John Doe",
+//   age: 30,
+//   gender: "male",
+//   country: "NGN",
+// };
+// const {name: personName, age: personAge, country: personCountry} = person;
+// console.log(personName);
+// console.log(personAge);
+// console.log(personCountry);
+
+
+// #7.8. FUNCTION DESTRUCTURING.
+function printPersonInfo(person) {
+    
+
+}
